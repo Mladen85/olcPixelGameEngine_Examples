a229fc9d5731dbf9fd4c385cd28f3da388b00e6c
Mladen <58631567+Mladen85@users.noreply.github.com>
2026-05-17 20:34:25 +0200

    story-01: Enabling test execution (#6) (#5)
    Fixes to enable test execution for "Red" phase (create positive test
    that fails when functionality is not implemented). "Red" phase is used
    to prove that test does not depend on implementation.

--- code/lib-physics/src/Ball.cpp
+++ code/lib-physics/src/Ball.cpp
@@ -1,1 +1,17 @@
 #include "Ball.hpp"
+
+Ball::Ball(olc::vf2d initPosition, olc::vf2d initVelocity, float radius) :
+    m_position(initPosition),
+    m_velocity(initVelocity),
+    m_radius(radius)
+{
+    //
+}
+
+void Ball::Update(float deltaTime) {
+    (void)deltaTime;
+}
+
+const olc::vf2d& Ball::GetPosition() const {
+    return m_position;
+}
--- code/test/src/BallTest.cpp
+++ code/test/src/BallTest.cpp
@@ -1,4 +1,21 @@
 #include <gtest/gtest.h>
 #include "Ball.hpp"
-#include "olcPixelGameEngine.h"
+// #include "olcPixelGameEngine.h"
 
+TEST(BallPhysicsTest, PositionChangesLinearlyWithVelocityAndDeltaTime) {
+    olc::vf2d initPosition(100.0f, 100.0f);
+    olc::vf2d initVelocity(10.0f, 10.0f);
+    float radius = 10.0f;
+
+    Ball ball(initPosition, initVelocity, radius);
+
+    // Game time advances 2 seconds
+    float deltaTime = 2.0f;
+    ball.Update(deltaTime);
+    
+    // Assert; Verify ball moved to new (expected) position
+    // newX = 100.0f + (10.0f * 2.0f) = 120.0f
+    // newY = 100.0f + (10.0f * 2.0f) = 120.0f
+    EXPECT_FLOAT_EQ(ball.GetPosition().x, 120.0f);
+    EXPECT_FLOAT_EQ(ball.GetPosition().y, 120.0f);
+}
